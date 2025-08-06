ae84a5a5a1a61aab7eab5ab93748eaf34adb4bc6
Hrishikesh Konwar <53933663+Hrishikesh-konwar@users.noreply.github.com>
2025-08-06 10:42:33 +0530

    remove

--- index.js
+++ index.js
@@ -30,7 +30,7 @@ mongoose
     console.error("MongoDB connection error:", err);
   });
 
-  console.log("MONGODB_URI:", process.env.MONGODB_URI);
+  // console.log("MONGODB_URI:", process.env.MONGODB_URI);
 
 
 app.use(express.json());
